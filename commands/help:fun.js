1940e22f05eba01edaf80d45e63e96f912d7a85d
Goseale <37878492+Goseale@users.noreply.github.com>
2018-06-01 19:20:45 -0300

    Update +8ball

--- commands/help:fun.js
+++ commands/help:fun.js
@@ -2,5 +2,6 @@ module.exports = async function(client, message, cmd, args) {
     message.channel.send(`\`\`\`html
 <flipcoin>            Flips a coin
 <random>              Chooses from a list
+<8ball>               Ask a question to the 8ball
 \`\`\``)
 }
