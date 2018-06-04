618335baaf7c9fa7129a8cf0774a3cddb5ba3044
Goseale <37878492+Goseale@users.noreply.github.com>
2018-06-04 20:04:39 -0300

    Update serverRoles.js

--- commands/serverRoles.js
+++ commands/serverRoles.js
@@ -3,6 +3,11 @@ module.exports = async function(client, message, cmd, args, prefix) {
   
   try{
 let roles = message.guild.roles.map(r => `<@&${r.id}>`).join('\n')
+if (roles.length >= 1024) {
+      message.channel.send("output too long, this means you have more than 1024 characters")
+      return
+
+
                   let richEmbed = new Discord.RichEmbed()
                   .setTitle(`Server roles list:`)
                   .setDescription(`${roles}`)
