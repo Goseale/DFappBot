315e3f2f1717e48c602d83180f9966db5c12a30f
Goseale <37878492+Goseale@users.noreply.github.com>
2018-06-04 20:01:05 -0300

    Update serverRoles.js

--- commands/serverRoles.js
+++ commands/serverRoles.js
@@ -1,8 +1,14 @@
 const Discord = require('discord.js');
 module.exports = async function(client, message, cmd, args, prefix) {
+  
+  try{
 let roles = message.guild.roles.map(r => `<@&${r.id}>`).join('\n')
                   let richEmbed = new Discord.RichEmbed()
                   .setTitle(`Server roles list:`)
                   .setDescription(`${roles}`)
                   message.channel.send({embed: richEmbed})
+  } catch(err) {
+    console.log(err)
+}
+  
 }
