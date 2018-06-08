290e32d67bc09798c0a6ea85457c2dc9dd3d498c
Goseale <37878492+Goseale@users.noreply.github.com>
2018-06-07 21:35:34 -0300

    Update info.js

--- commands/info.js
+++ commands/info.js
@@ -15,7 +15,7 @@ let ms = client.uptime
     const Embed = new Discord.RichEmbed()
             .setTitle("Bot info")
             .addField("Why this bot was made?", `Because i wanted to own a bot :V and also i wanted to know javascript and the best was to do it is creating a discord bot.Im constantly uptading it with new features, to make it the best bot ever!!`, true)
-            .addField("Memory usage", `${math.round.memory} MB`, true)
+            .addField("Memory usage", `${Math.round.memory} MB`, true)
             .addField("Uptime", `**Ms:**\`${ms}\`, **Seconds:**\`${seconds}\`, **Minutes:**\`${minutes}\`, **Hours:**\`${hours}\``, true)
             .setColor(667110);
   message.channel.send({embed: Embed});
