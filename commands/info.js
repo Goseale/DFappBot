88661ed2a681b4acf2598eb718a3865bb8bf218a
Goseale <37878492+Goseale@users.noreply.github.com>
2018-06-07 21:19:21 -0300

    Update info.js

--- commands/info.js
+++ commands/info.js
@@ -10,6 +10,8 @@ let ms = client.uptime
     seconds = seconds % 60
     minutes = minutes % 60
     
+    
+    try{
     const Embed = new Discord.RichEmbed()
             .setTitle("Bot info")
             .addField("Why this bot was made?", `Because i wanted to own a bot :V and also i wanted to know javascript and the best was to do it is creating a discord bot.Im constantly uptading it with new features, to make it the best bot ever!!`, true)
@@ -17,5 +19,9 @@ let ms = client.uptime
             .addField("Uptime", `**Ms:**\`${ms}\`, **Seconds:**\`${seconds}\`, **Minutes:**\`${minutes}\`, **Hours:**\`${hours}\``, true)
             .setColor(667110);
   message.channel.send({embed: Embed});
+    } catch(err) {
+        console.log(err)
+        return
+    }
     
 }
