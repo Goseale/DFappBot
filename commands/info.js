fa110e9f29b0bed3e0fb28737c925579419cf12b
Goseale <37878492+Goseale@users.noreply.github.com>
2018-06-07 21:58:15 -0300

    Update info.js

--- commands/info.js
+++ commands/info.js
@@ -9,7 +9,6 @@ let ms = client.uptime
     ms = ms % 1000
     let minutes = Math.floor(seconds / 60)
     let hours = Math.floor(minutes / 60)
-    let memory = (process.memoryUsage().heapUsed / 1024 / 1024)
     seconds = seconds % 60
     minutes = minutes % 60
     
@@ -18,7 +17,7 @@ let ms = client.uptime
     const Embed = new Discord.RichEmbed()
             .setTitle("Bot info")
             .addField("Why this bot was made?", `Because i wanted to own a bot :V and also i wanted to know javascript and the best was to do it is creating a discord bot.Im constantly uptading it with new features, to make it the best bot ever!!`, true)
-            .addField("Memory usage", `${Math.round.memory} MB`, true)
+            .addField("Memory Usage", `${(process.memoryUsage().heapUsed / 1024 / 1024).toFixed(2)} MB`, true)
             .addField("Node Version", process.version, true)
             .addField("NPM Version", npmv.data.replace("\n", ""), true)
             .addField('OS', `${os.platform()} (${process.arch})`, true)
