533423f94b2cb7b83edb2362c9f55647b80fee85
Goseale <37878492+Goseale@users.noreply.github.com>
2018-06-24 20:45:25 -0300

    Reverted

--- commands/eval.js
+++ commands/eval.js
@@ -80,9 +80,7 @@ module.exports = async function(client, message, cmd, args, prefix) {
       ${e}\`\`\``
       },
           ]
-    }}).then(m => {let response = m.createdTimestamp - message.createdTimestamp
-    message.channel.send(`Evaled in: ${response} ms`)
-                  })
+    }})
   })
   
   
