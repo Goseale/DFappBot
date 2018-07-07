e780391c4073ae0c12db80362f4b04f2bc0464da
Goseale <37878492+Goseale@users.noreply.github.com>
2018-07-06 21:19:54 -0300

    Update signal.js

--- events/signal.js
+++ events/signal.js
@@ -1,10 +1,15 @@
-
+module.exports = {
+  async main(client) {
 
 
 function handle(signal) {
+  console.log(" ")
   console.log(`Received ${signal}`);
+  console.log(" ")
+  client
   client.destroy()
 }
 
 process.on('SIGINT', handle);
 process.on('SIGTERM', handle);
+  }
