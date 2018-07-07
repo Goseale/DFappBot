91db22960ce4c328b2d69907caa30761c32acc5f
Goseale <37878492+Goseale@users.noreply.github.com>
2018-07-06 21:30:06 -0300

    Signal event

--- events/signal.js
+++ events/signal.js
@@ -2,11 +2,19 @@ module.exports = {
   async main(client) {
 
 let hiddenlog = client.channels.get(`357596301522632715`);
+var currentdate = new Date(); 
+var datetime = "Event on: " + currentdate.getDate() + "/"
+                + (currentdate.getMonth()+1)  + "/" 
+                + currentdate.getFullYear() + " @ "  
+                + currentdate.getHours() + ":"  
+                + currentdate.getMinutes() + ":" 
+                + currentdate.getSeconds();
+    
 function handle(signal) {
   console.log(" ")
   console.log(`Received ${signal}`);
   console.log(" ")
-  if (hiddenlog) hiddenlog.send(`Received ${signal}\nClient has been terminated.`)
+  if (hiddenlog) hiddenlog.send(`Received ${signal}\nClient has been terminated.\n${datetime}`)
   client.destroy()
 }
 
