4a6d268b56f505e6e64e4ac4848142229bb48254
Goseale <37878492+Goseale@users.noreply.github.com>
2018-08-28 22:32:37 -0300

    Update help:general.js

--- commands/help:general.js
+++ commands/help:general.js
@@ -1,11 +1,7 @@
 module.exports = async function(client, message, cmd, args) {
     message.channel.send(`\`\`\`html
 <ping>           Tests the delay of the bot
-<invite>         Shows the invitation link of the bot and also the invitation to the bot´s server.
 <say>            Make the bot say something
-<messagebox [Language] [Code]> Send code using a code box
 <embed>          Shows an embed usage [embed [title] [description]
-<test>           Testing command. Avalible for the owner of the bot
-<MCserver [IP]>  Information of a Minecraft server
 \`\`\``)
 }
