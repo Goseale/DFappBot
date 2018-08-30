09951041dc2813936bce3cba62c4b819fa2f641f
Goseale <37878492+Goseale@users.noreply.github.com>
2018-08-30 07:03:08 -0300

    no fake msgs

--- commands/say.js
+++ commands/say.js
@@ -1,5 +1,8 @@
 module.exports = async function(client, message, cmd, args, prefix) {
 
+      let ownerID = process.env.OWNER
+      let littleID = 230880116035551233
+      
       if (!args[0]) {
     const Discord = require('discord.js');
     const Error = new Discord.RichEmbed()
@@ -12,6 +15,7 @@ module.exports = async function(client, message, cmd, args, prefix) {
     return
   }
     message.delete()
+    
+    if(message.author.id !== ownerID && message.author.id !== littleID) return message.channel.send(`${message.author.username}: ${args.join(' ')}`)
     message.channel.send(args.join(' '))
-
 }
